b43ea0da186ded02edf8b877d52619dd552ff3a4
Tina Tram <tinatram07@gmail.com>
2025-01-30 17:01:06 -0600

    CSS setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    console.log("works")
+})
